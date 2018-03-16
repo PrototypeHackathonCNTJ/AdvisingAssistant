2bbe5e4c6a16ab5f05efd9a8ed301e9797df73bc
Jacob Misirian <misirianrule@gmail.com>
2018-03-16 18:42:12 -0500

    Removed useless file and Offering.cs

--- src/AdvisingAssistant/Program.cs
+++ src/AdvisingAssistant/Program.cs
@@ -14,9 +14,19 @@ namespace AdvisingAssistant.Courses
         public static void Main(string[] args)
         {
             string json = File.ReadAllText(args[0]);
-            Course c = new Course(json);
-            Console.WriteLine(c.ID);
-            Console.WriteLine(c.Name);
+
+            while (json.IndexOf('}') != -1)
+            {
+                int nextEndBrace = json.IndexOf('}');
+                string obj = json.Substring(0, nextEndBrace + 1);
+                json = json.Substring(nextEndBrace + 1);
+
+                Course c = new Course(obj);
+                Console.WriteLine(c.Name);
+                Console.WriteLine(c.Credits);
+                Console.WriteLine(c.Info);
+                Console.WriteLine();
+            }
         }
     }
 }
