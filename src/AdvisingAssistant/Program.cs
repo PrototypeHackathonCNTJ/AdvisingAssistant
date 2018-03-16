da63a33f9b165363255a6c8d220a045f80e126ab
Jacob Misirian <misirianrule@gmail.com>
2018-03-16 16:00:43 -0500

    Initial Commit.

---
+++ src/AdvisingAssistant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdvisingAssistant
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
