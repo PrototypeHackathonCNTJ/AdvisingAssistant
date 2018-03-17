b99973137fec910c9770b02e1b620eab71f0f957
Jacob Misirian <misirianrule@gmail.com>
2018-03-16 22:50:31 -0500

    FUCK

--- src/AdvisingAssistant/Program.cs
+++ src/AdvisingAssistant/Program.cs
@@ -23,10 +23,10 @@ namespace AdvisingAssistant.Courses
                 string obj = json.Substring(0, nextEndBrace + 1);
                 json = json.Substring(nextEndBrace + 1);
 
-                Course c = new Course(obj);
+                Optional c = new Optional(obj);
                 Console.WriteLine(c.Name);
                 //Console.WriteLine(c.Credits);
-                Console.WriteLine(c.Info);
+                //Console.WriteLine(c.Info);
                 Console.WriteLine();
             }
         }
