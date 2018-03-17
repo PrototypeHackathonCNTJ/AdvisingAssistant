772964fd4db6f08d0fc970f3bac6f2822c8822d0
Jacob Misirian <misirianrule@gmail.com>
2018-03-16 22:11:03 -0500

    Removed Cole 's shit

--- src/AdvisingAssistant/Program.cs
+++ src/AdvisingAssistant/Program.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 
 using AdvisingAssistant.Courses;
+using AdvisingAssistant.Majors;
+using AdvisingAssistant.CourseOptionals;
 
 using Newtonsoft.Json;
 
@@ -21,10 +23,10 @@ namespace AdvisingAssistant.Courses
                 string obj = json.Substring(0, nextEndBrace + 1);
                 json = json.Substring(nextEndBrace + 1);
 
-                Course c = new Course(obj);
+                Optional c = new Optional(obj);
                 Console.WriteLine(c.Name);
                 Console.WriteLine(c.Credits);
-                Console.WriteLine(c.Info);
+                //Console.WriteLine(c.Info);
                 Console.WriteLine();
             }
         }
